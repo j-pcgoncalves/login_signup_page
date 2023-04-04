483cbd9c9089802960ef1f19960e6e42ce3a12bc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-04 22:52:05 +0100

    create a responsive file

---
+++ lib/Screens/Welcome/components/login_signup_btn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginAndSignupBtn extends StatelessWidget {
+  const LoginAndSignupBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_page/Screens/Welcome/components/login_signup_btn.dart';
+import 'package:login_signup_page/Screens/Welcome/components/welcome_image.dart';
 import 'package:login_signup_page/components/background.dart';
+import 'package:login_signup_page/responsive.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Background(
-      child: SingleChildScrollView(),
+    return Background(
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Responsive(
+            desktop: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Expanded(
+                  child: WelcomeImage(),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SizedBox(
+                        width: 450,
+                        child: LoginAndSignupBtn(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            mobile: const MobileWelcomeScreen(),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class MobileWelcomeScreen extends StatelessWidget {
+  const MobileWelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }

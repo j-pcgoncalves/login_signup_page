948b5443da4011efd23b7c18dfb0c220bb4326d0
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-04 23:31:17 +0100

    finish loginScreen

--- lib/Screens/Login/components/login_form.dart
+++ lib/Screens/Login/components/login_form.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_page/Screens/Signup/signup_screen.dart';
+import 'package:login_signup_page/components/already_have_an_account_check.dart';
+import 'package:login_signup_page/constants.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: 'Your email',
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Your password',
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          Hero(
+            tag: 'login_btn',
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Login'.toUpperCase(),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(press: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const SignUpScreen();
+                },
+              ),
+            );
+          }),
+        ],
+      ),
+    );
   }
 }

0a2fa28e93111f82b766c96ec6440b3838b9e74a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-04 23:04:09 +0100

    style login btns

--- lib/Screens/Welcome/components/login_signup_btn.dart
+++ lib/Screens/Welcome/components/login_signup_btn.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_page/Screens/Login/login_screen.dart';
+import 'package:login_signup_page/Screens/Signup/signup_screen.dart';
+import 'package:login_signup_page/constants.dart';
 
 class LoginAndSignupBtn extends StatelessWidget {
   const LoginAndSignupBtn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Hero(
+          tag: 'login_btn',
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const LoginScreen();
+                  },
+                ),
+              );
+            },
+            child: Text(
+              'Login'.toUpperCase(),
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const SignUpScreen();
+                },
+              ),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kPrimaryLightColor, elevation: 0),
+          child: Text(
+            'Sign Up'.toUpperCase(),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
   }
 }

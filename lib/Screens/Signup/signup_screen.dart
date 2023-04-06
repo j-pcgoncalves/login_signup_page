252da970197a088f43ddb1859f4b318b9715ee24
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-06 18:20:49 +0100

    create SignUpScreenTopImage

--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_page/Screens/Signup/components/sign_up_top_image.dart';
+import 'package:login_signup_page/components/background.dart';
+import 'package:login_signup_page/responsive.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: const MobileSignupScreen(),
+          desktop: Row(),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileSignupScreen extends StatelessWidget {
+  const MobileSignupScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const SignUpScreenTopImage(),
+      ],
+    );
   }
 }

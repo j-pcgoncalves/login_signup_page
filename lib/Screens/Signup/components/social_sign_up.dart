9bf8fcdd9d3d1b722ab3fd22212ba925a0e4898e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-06 18:40:36 +0100

    create and style SocialIcon

--- lib/Screens/Signup/components/social_sign_up.dart
+++ lib/Screens/Signup/components/social_sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_signup_page/Screens/Signup/components/or_divider.dart';
+import 'package:login_signup_page/Screens/Signup/components/social_icon.dart';
 
 class SocialSignUp extends StatelessWidget {
   const SocialSignUp({Key? key}) : super(key: key);
@@ -7,8 +8,25 @@ class SocialSignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        OrDivider(),
+      children: [
+        const OrDivider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SocialIcon(
+              iconSrc: 'assets/icons/facebook.svg',
+              press: () {},
+            ),
+            SocialIcon(
+              iconSrc: 'assets/icons/twitter.svg',
+              press: () {},
+            ),
+            SocialIcon(
+              iconSrc: 'assets/icons/google-plus.svg',
+              press: () {},
+            ),
+          ],
+        ),
       ],
     );
   }
